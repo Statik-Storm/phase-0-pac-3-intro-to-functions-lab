ce117cb046054f81ac2a45a25bf20508c02adb12
Steven <statikstorm@outlook.com>
2024-10-14 17:09:12 -0700

    myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,23 @@
 require ( './root.js' );
 
 
-describe('shout(string)', function() {
+describe('shout(string.toUpperCase());', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
 })
 
-describe('whisper(string)', function() {
+describe('whisper(string.toLowerCase());', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
 
-describe('logShout(string)', function() {
+describe('logShout(string.toUpperCase())';, function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
 
-    logShout('hello');
+        logShout('HELLO');
 
     expect(spy).toHaveBeenCalledWith('HELLO');
 
@@ -25,11 +25,11 @@ describe('logShout(string)', function() {
   })
 })
 
-describe('logWhisper(string)', function() {
+describe('logWhisper(string.toLowerCase());', function() {
   it('takes a string argument and logs it in all lowercase using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
 
-    logWhisper('HELLO');
+    logWhisper('hello');
 
     expect(spy).toHaveBeenCalledWith('hello');
 
@@ -37,11 +37,11 @@ describe('logWhisper(string)', function() {
   })
 })
 
-describe('sayHiToHeadphonedRoommate(string)', function() {
+describe('sayHiToHeadphonedRoommate(string.toLowerCase());', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
   })
-
+describe('sayHiToHeadphonedRoommate(string.toUpperCase());', function() {
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
   })
